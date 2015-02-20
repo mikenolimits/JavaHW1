aa41c899b6d78413f0f66c5b9e4b9acc3bd61980
mikenolimits <empathynyc@gmail.com>
2015-02-20 16:36:48 -0500

    remove stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,21 +9,28 @@ public class Main {
         // write your code here
 
 
+        //We create an infinite loop that keeps going until the exit condition is met.
         while (true) {
 
+            //Counters to keep track of everything set to 0 on every loop.
             int uppercase = 0;
             int lowerCase = 0;
             int digits    = 0;
+            Boolean exitCondition = false;
 
-            String sentance = JOptionPane.showInputDialog("Please Type In a Sentence");
+            String input    = JOptionPane.showInputDialog("Please Type In a Sentence");
+            exitCondition   = input.equalsIgnoreCase("stop");
 
-            if (sentance.equalsIgnoreCase("stop")) {
+            if (exitCondition) {
                 break;
             }
+            //loop up to the size of the input so that we can get the current
+            //character for every iteration and increment the counter for whatever
+            //condition they meet.
 
-            for (int i = 0; i <= sentance.length() - 1; i++) {
+            for (int i = 0; i <= input.length() - 1; i++) {
 
-                char current = sentance.charAt(i);
+                char current = input.charAt(i);
 
                 if (Character.isUpperCase(current)) {
                     uppercase++;
@@ -36,9 +43,10 @@ public class Main {
                 }
 
             }
+            //Show a brutally annoying message dialog for every counter :P
             showCounter(uppercase, "uppercase");
             showCounter(lowerCase, "lowercase");
-            showCounter(digits,       "digits");
+            showCounter(digits,    "digits");
 
         }
     }
